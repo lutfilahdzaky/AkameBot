ed3934e6acc9c5be49073913569787ed69c1fe91
Rizkyawan028 <58576231+Rizkyawan028@users.noreply.github.com>
2020-02-11 23:53:05 +0800

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -123,7 +123,7 @@ async def pipcheck(pip):
 async def amireallyalive(on):
     """ For .on command, check if the bot is running.  """
     await on.edit("`"
-                     "工ㄥ卄丹爪_爪丹uㄥ丹n丹艾 \n"
+                     "Rizkyーデス \n"
                      "Bot Siap Boss! \n"
                      f"------------------------------------ \n"
                      f"⡆⣐⢕⢕⢕⢕⢕⢕⢕⢕⠅⢗⢕⢕⢕⢕⢕⢕⢕⠕⠕⢕⢕⢕⢕⢕⢕⢕⢕⢕ \n"
@@ -145,7 +145,8 @@ async def amireallyalive(on):
                      f"Python: {python_version()} \n"
                      f"------------------------------------ \n"
                      f"Bio: {DEFAULTUSER} \n"
-		             f"Username: @Ilham_Maulana1 \n"
+		             f"Username: @AkameNFS \n"
+f"Bot Tampan 😎 \n"
 		             f"------------------------------------ \n"
                      "`")
 
