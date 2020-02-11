1284095215241124e6952876c8a6e3c1c44957fa
Rizkyawan028 <58576231+Rizkyawan028@users.noreply.github.com>
2020-02-11 20:30:12 +0800

    Update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -129,19 +129,19 @@ async def fetch_info(replied_user, event):
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
 
-    caption = "<b>USER INFO:</b>\n\n"
-    caption += f"First Name: {first_name}\n"
-    caption += f"Last Name: {last_name}\n"
+    caption = "<b>INFO SI BANGSAT:</b>\n\n"
+    caption += f"Nama Pertama: {first_name}\n"
+    caption += f"Nama Akhir: {last_name}\n"
     caption += f"Username: {username}\n"
-    caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"Is Bot: {is_bot}\n"
-    caption += f"Is Restricted: {restricted}\n"
-    caption += f"Is Verified by Telegram: {verified}\n"
+    caption += f"Data ID: {dc_id}\n"
+    caption += f"Jumlah Foto Profil: {replied_user_profile_photos_count}\n"
+    caption += f"Apakah Bot: {is_bot}\n"
+    caption += f"Apakah Restricted: {restricted}\n"
+    caption += f"Terferifikasi oleh Telegram: {verified}\n"
     caption += f"ID: <code>{user_id}</code>\n\n"
     caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n"
-    caption += f"Permanent Link To Profile: "
+    caption += f"Jumlah Grup Yang Sama: {common_chat}\n"
+    caption += f"Link Permanen Profile: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
     return photo, caption
