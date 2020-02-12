8496c4fd6a340eab7df06e5ac6c36e0d37d1fe6a
Rizkyawan028 <58576231+Rizkyawan028@users.noreply.github.com>
2020-02-12 09:49:29 +0800

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -237,7 +237,7 @@ async def kang(args):
                 await bot.send_read_acknowledge(conv.chat_id)
 
         await args.edit(f"`Sticker kanged successfully!`\
-            \nPack Milik @AkameNFS Klik [disini](t.me/addstickers/{packname})",
+            \nStiker Ditambahkan, Klik [disini](t.me/addstickers/{packname})",
                         parse_mode='md')
 
 
