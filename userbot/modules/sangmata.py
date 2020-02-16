590110cff283e2e6230802aba95e0098cc79530b
Rizkyawan028 <58576231+Rizkyawan028@users.noreply.github.com>
2020-02-16 09:27:48 +0800

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -34,7 +34,7 @@ async def _(event):
               await event.reply("`Please unblock @sangmatainfo_bot and try again`")
               return
           if response.text.startswith("Forward"):
-             await event.edit("`can you kindly disable your forward privacy settings for good?`")
+             await event.edit("`Akun Disembunyikan Oleh Pengguna Tidak Bisa Meneruskan Pesan!`")
           else: 
              await event.edit(f"{response.message.message}")
 
