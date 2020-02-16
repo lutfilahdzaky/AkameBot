42dccf0418c7f841eca86af871f69293e44ffdeb
Rizkyawan028 <58576231+Rizkyawan028@users.noreply.github.com>
2020-02-16 09:08:01 +0800

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -12,28 +12,28 @@ async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
+       await event.edit("```Balas Ke Pesan Siapapun.```")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit("```Reply to text message```")
+       await event.edit("```Balas Ke Teks```")
        return
     chat = "@QuotLyBot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
+       await event.edit("```Balas Ke Pesan Pengguna.```")
        return
-    await event.edit("```Making Love```")
+    await event.edit("```Membuat QuotLy```")
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
               await bot.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock @QuotLyBot and try again```")
+              await event.reply("```Tolong unblock @QuotLyBot dan coba lagi```")
               return
           if response.text.startswith("Hi!"):
-             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
+             await event.edit("```Bisakah Kamu Meminta Kepada Admin Untuk Mengizinkan Foward?```")
           else: 
              await event.delete()   
              await bot.forward_messages(event.chat_id, response.message)
