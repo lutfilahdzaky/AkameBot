e107017a7a095edc695fb6f12b052836d0d9d0b6
AkameNFS <58576231+RizkyNFS@users.noreply.github.com>
2020-03-03 15:39:07 +0800

    Create p.py

---
+++ userbot/modules/p.py
@@ -0,0 +1,19 @@
+import asyncio
+import re
+import time
+from time import sleep
+from userbot import CMD_HELP, ZALG_LIST
+from userbot.events import register
+
+@register(outgoing=True, pattern='^.fl(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	await typew.edit("`Ucap Salam Goblok...`")
+	sleep(1)
+	await typew.edit("`Assalamualaikum`")
+
+
+
+
+
+
