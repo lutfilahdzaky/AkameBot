1152cebd0a423e02bf979708f708e9afce0e90b8
AkameNFS <58576231+RizkyNFS@users.noreply.github.com>
2020-03-03 15:40:27 +0800

    Update p.py

--- userbot/modules/p.py
+++ userbot/modules/p.py
@@ -5,7 +5,7 @@ from time import sleep
 from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
-@register(outgoing=True, pattern='^.fl(?: |$)(.*)')
+@register(outgoing=True, pattern='^P(?: |$)(.*)')
 async def typewriter(typew):
 	message = typew.pattern_match.group(1)
 	await typew.edit("`Ucap Salam Goblok...`")
