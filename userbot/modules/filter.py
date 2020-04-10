f18ad5ecd7d6a3c2a3368294b9a635215221ed11
AkameNFS <58576231+RizkyNFS@users.noreply.github.com>
2020-04-10 17:07:40 +0800

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -11,7 +11,7 @@ from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 
 
-@register(incoming=True, disable_edited=True, disable_errors=True)
+@register(incoming=True, disable_edited=False, disable_errors=True)
 async def filter_incoming_handler(handler):
     """ Checks if the incoming message contains handler of a filter """
     try:
