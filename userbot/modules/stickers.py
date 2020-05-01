4f5e95c510a91643a02d13ef06ce60e38c54ae97
AkameNFS <58576231+RizkyNFS@users.noreply.github.com>
2020-05-01 20:43:21 +0800

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -96,7 +96,7 @@ async def kang(args):
                 emoji = splat[1]
 
         packname = f"a{user.id}_by_{user.username}_{pack}"
-        packnick = f"Reemar Martin♥️ by @AkameNFS"
+        packnick = f"@{user.username}'s kang pack Vol.{pack}"
         cmd = '/newpack'
         file = io.BytesIO()
 
@@ -124,7 +124,7 @@ async def kang(args):
                 while "120" in x.text:
                     pack += 1
                     packname = f"a{user.id}_by_{user.username}_{pack}"
-                    packnick = f"Reemar Martin♥️ by @AkameNFS"
+                    packnick = f"@{user.username}'s kang pack Vol.{pack}"
                     await args.edit("`Switching to Pack " + str(pack) +
                                     " due to insufficient space`")
                     await conv.send_message(packname)
