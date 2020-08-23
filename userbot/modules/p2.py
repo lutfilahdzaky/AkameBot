cfde7f9f5c12f40d8f4a0c1f463fc619d006a4e5
Rizkyawan Ikkyy <58576231+RizkyAkame@users.noreply.github.com>
2020-08-23 13:29:59 +0800

    Update p2.py

--- userbot/modules/p2.py
+++ userbot/modules/p2.py
@@ -15,6 +15,15 @@ async def typewriter(typew):
 # Create by myself @AkameNFS
 
 
+@register(outgoing=True, pattern='^P(?: |$)(.*)')
+async def typewriter(typew):
+	message = typew.pattern_match.group(1)
+	sleep(1)
+	await typew.edit("Biasakanlah Mengucapkan Salam")
+	sleep(1)
+	await typew.edit("Assalamualaikum")
+# Create by myself @AkameNFS
+
 
 
 
